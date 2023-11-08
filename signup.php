470a3ab15758c107dd749c426e36a01890caea7f
vchan66 <vilanchan6666@gmail.com>
2023-11-08 21:20:08 +0800

    same email cannot register twice

--- signup.php
+++ signup.php
@@ -14,11 +14,25 @@ $password = $_POST['password'];
 $password2 = $_POST['confirmPassword'];
 
 // echo ("$username" . "<br />". "$password2" . "<br />");
+
+
+$query = "SELECT * FROM users";
+$result = $db->query($query);
+
+while ($rows = $result ->fetch_assoc()) {
+	if ($rows['email'] == $email){
+		echo "<script> alert('You already have an account with Leafy Bites. Please login with your existing account.')</script>";
+		echo '<script> location.href = "signup.html"</script> ';
+		exit;
+	}
+}
+
 if ($password != $password2) {
-	echo "<script> alert('Sorry, passwords do not match')</script>";
+	echo "<script> alert('Passwords do not match. Please retry.')</script>";
 	echo '<script> location.href = "signup.html"</script> ';
 	exit;
-	}
+}
+
 $password = md5($password);
 // echo $password;
 $sql = "INSERT INTO users (username, email, password) 
